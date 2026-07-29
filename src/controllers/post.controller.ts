5a4566524b21efc84249a82ec6a7d603cdb6042b
Fakile Razaq <razaq_jimmie@yahoo.com>
2026-07-29 19:57:29 +0100

    Error Handler Middleware

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,6 +1,5 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { createNewPost, getAllPosts } from "../service/post.service.js";
-import { Prisma } from "../generated/prisma/client.js";
 
 export const getPosts = async (req: Request, res: Response) => {
 	try {
@@ -12,20 +11,12 @@ export const getPosts = async (req: Request, res: Response) => {
 	}
 };
 
-export const createPost = async(req: Request, res: Response) => {
+export const createPost = async(req: Request, res: Response, next: NextFunction) => {
 	try {
-
 		const post = await createNewPost(req.body);
-		res.status(201).json(post);	
 
+		return res.status(201).json(post);
 	} catch (error) {
-		console.error(error);
-
-		if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2003") {
-			return res.status(400).json({
-				message: "The specified user does not exist."
-			});
-		}
-		return res.status(500).json({ message: "Internal Server Error"});
+		next(error);
 	}	
-}
+};
---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+export const errorHandler = 
+(error: Error, req: Request, res: Response, next: NextFunction) => {
+
+	console.log(error);
+
+	return res.status(500).json({
+		message: "Internal Server Error"
+	});
+};
