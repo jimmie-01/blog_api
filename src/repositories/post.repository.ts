52558bd429743fc64a42f30e34a3fc0dd6599d7e
Fakile Razaq <razaq_jimmie@yahoo.com>
2026-08-02 09:06:16 +0100

    Update post bug fix

--- src/repositories/post.repository.ts
+++ src/repositories/post.repository.ts
@@ -1,6 +1,7 @@
 import prisma from "../lib/prisma.js";
 import { CreatePostDto } from "../dto/create-post.dto.js";
 import { UpdatePostDto } from "../dto/update-post.dto.js";
+import { Prisma } from "../generated/prisma/client.js";
 
 export const findAllPosts = async () => {
 	return prisma.posts.findMany();
@@ -14,5 +15,15 @@ export const createPost = async (data: CreatePostDto) => {
 
 export const updatePost = async (id: number, data: UpdatePostDto) => {
 
-	return await prisma.posts.update({ where: { id }, data })
+	const updateData: Prisma.postsUpdateInput = {};
+
+	if (data.title !== undefined) {
+		updateData.title = data.title;
+	}
+
+	if (data.content !== undefined) {
+		updateData.content = data.content;
+	}
+
+	return await prisma.posts.update({ where: { id }, data: updateData })
 }
