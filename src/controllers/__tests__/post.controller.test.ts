c5439d3cf70a50d85900f6b4c981bb385ba22c8c
Fakile Razaq <razaq_jimmie@yahoo.com>
2026-08-11 18:54:24 +0100

    Controller Create Post Unit Test

--- src/controllers/__tests__/post.controller.test.ts
+++ src/controllers/__tests__/post.controller.test.ts
@@ -4,3 +4,37 @@ import * as service from "../../service/post.service.js";
 
 vi.mock("../../service/post.service.js");
 
+describe("Post Controller", () => {
+	it("should return 201 and the created post when a valid data is provided", async () => {
+		const req = {
+			body: {
+				title: "Learning Testing",
+				content: "Controller are fun.",
+				user_id: 1
+			}
+		} as unknown as Request;
+
+		const res = {
+			status: vi.fn().mockReturnThis(),
+			json: vi.fn()
+		} as unknown as Response;
+
+		const next = vi.fn();
+
+		const fakePost = {
+			id: 1,
+			title: "Learning Unit Testing"
+		};
+
+		vi.spyOn(service, "createNewPost").mockResolvedValue(fakePost);
+
+		await createPost(req, res, next);
+
+		expect(service.createNewPost).toHaveBeenCalledWith(req.body);
+		// This proves the controller called the sevice correctly
+
+		expect(res.status).toHaveBeenCalledWith(201);
+
+		expect(res.json).toHaveBeenCalledWith(fakePost);
+	});
+})
