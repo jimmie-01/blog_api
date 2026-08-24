c6bbc9710888e4fb382172c4bf23cb722ec70795
Fakile Razaq <razaq_jimmie@yahoo.com>
2026-08-24 11:01:21 +0100

    Password Hash Utility Unit Test

---
+++ src/utils/__tests__/password.test.ts
@@ -0,0 +1,39 @@
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import { hashPassword, comparePassword } from "../password.js";
+
+
+describe("Auth Password", () => {
+
+	it("should return a hashed password", async () => {
+		
+		const userPassword = "last123";
+
+		const result = await hashPassword(userPassword);
+
+		expect(result).not.toBe(userPassword);
+
+		expect(result).toBeTypeOf("string");
+	});
+
+	it("should create a hash that can be verified", async () => {
+		const userPassword = "user1234";
+
+		const passwordHash = await hashPassword(userPassword);
+
+		const result = await comparePassword(userPassword, passwordHash);
+
+		expect(result).toBe(true);
+	});
+
+	it("should reject an incorrect passworrd", async () => {
+		const userPassword = "user3456";
+
+		const wrongPassword = "wrongUser123";
+
+		const passwordHash = await hashPassword(userPassword);
+
+		const result = await comparePassword(wrongPassword, passwordHash);
+
+		expect(result).toBe(false);
+	})
+})
