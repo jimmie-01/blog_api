ccb70996119cc8e21e10b94931dbe1f7bf43178d
Fakile Razaq <razaq_jimmie@yahoo.com>
2026-08-31 11:38:17 +0100

    Auth Controller Register Integration test

--- src/integration/auth.integration.test.ts
+++ src/integration/auth.integration.test.ts
@@ -10,3 +10,49 @@ import request from "supertest";
 import app from "../app.js";
 import prisma from "../lib/prisma.js";
 import { cleanDatabase } from "./helpers/database.js";
+
+describe("POST /api/auth/register", () => {
+
+	beforeEach( async () => {
+		await cleanDatabase();
+	});
+
+	afterAll(async () => {
+		await cleanDatabase();
+
+		await prisma.$disconnect();
+	});
+
+	it("should register a new user", async () => {
+
+		const response = await request(app).
+		post("/api/auth/register").
+		send({
+			name: "Razaq",
+			email: "razaq@example.com",
+			username: "jimmie-01",
+			password: "user-password"
+		});
+
+		expect(response.status).toBe(201);
+
+		expect(response.body).toMatchObject({
+			message: "User created successfully",
+			user: response.body.user
+		});
+
+		const user = await prisma.users.findUnique({
+			where: {
+				email: response.body.user.email
+			}
+		});
+
+		expect(user).not.toBeNull();
+
+		expect(user).toMatchObject({
+			name: "Razaq",
+			email: "razaq@example.com",
+			username: "jimmie-01"
+		});
+	})
+})
