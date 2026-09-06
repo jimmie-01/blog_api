1bd180e1b80a96c4d78988943134604d1bc6e760
Fakile Razaq <razaq_jimmie@yahoo.com>
2026-09-06 12:05:04 +0100

    JWT configuration

---
+++ src/utils/token.ts
@@ -0,0 +1,27 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+
+interface TokenPayLoad{
+	userId: number
+};
+
+export const generateAccessToken = 
+(userId: number): string => {
+	const secret = process.env.JWT_SECRET;
+	const expiresIn = process.env.JWT_EXPIRES_IN;
+
+	if (!secret) {
+		throw new Error("JWT_SECRET is not configured");
+	}
+	
+	if (!expiresIn) {
+		throw new Error("JWT_EXPIRES is not configured")
+	}
+
+	return jwt.sign(
+		{ userId },
+		secret,
+		{ 
+			expiresIn: expiresIn as SignOptions["expiresIn"] 
+		}
+	);
+};
