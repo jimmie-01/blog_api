6e0db9aa60a846f2e882f65b26a352a1ec152be0
Fakile Razaq <razaq_jimmie@yahoo.com>
2026-09-17 22:51:44 +0100

    Refresh Token Configuration

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,29 +1,64 @@
 import jwt, { SignOptions } from "jsonwebtoken";
 import type { UserRole } from "../generated/prisma/enums.js";
 
-interface TokenPayLoad{
+interface AccessTokenPayLoad{
 	userId: number;
 	role: UserRole;
+	type: "access"
 };
 
-export const generateAccessToken = 
-(userId: number, role: UserRole): string => {
+interface RefreshTokenPayLoad{
+	userId: number;
+	type: "refresh"
+}
+
+const getJwtSecret = (): string => {
 	const secret = process.env.JWT_SECRET;
-	const expiresIn = process.env.JWT_EXPIRES_IN;
 
 	if (!secret) {
 		throw new Error("JWT_SECRET is not configured");
 	}
+
+	return secret;
+}
+
+export const generateAccessToken = 
+(userId: number, role: UserRole): string => {
+	const secret = getJwtSecret();
+	const expiresIn = process.env.JWT_EXPIRES_IN || "15m";
 	
 	if (!expiresIn) {
 		throw new Error("JWT_EXPIRES_IN is not configured")
 	}
 
 	return jwt.sign(
-		{ userId, role },
+		{ userId, 
+			role, 
+			type: "access"
+		},
 		secret,
 		{ 
 			expiresIn: expiresIn as NonNullable<SignOptions["expiresIn"]> 
 		}
 	);
 };
+
+export const generateRefreshToken = (
+	userId: number
+): string => {
+	const expiresIn = process.env.JWT_REFRESH_EXPIRES_IN || "7d";
+
+	if (!expiresIn) {
+		throw new Error("JWT_REFRESH_EXPIRES_IN is not configured");
+	}
+
+	return jwt.sign(
+		{userId,
+		type: "refresh", 
+	},
+	getJwtSecret(),
+	{
+		expiresIn: expiresIn as NonNullable<SignOptions["expiresIn"]>
+	}
+	);
+}
